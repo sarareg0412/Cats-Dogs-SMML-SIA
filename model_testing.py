d9cb0b8468fbd3f098bf5d92854ac423d75dd7fe
sara9 <sara9@MSISARA>
2022-08-21 18:40:14 +0200

    K Fold seems working

--- model_testing.py
+++ model_testing.py
@@ -1,29 +1,17 @@
 import matplotlib.pyplot as plt
 
 
-def plot_history(history):
-    '''Plot loss and accuracy as a function of the epoch,
-    for the training and validation datasets.
-    '''
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
-
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-
-#    epochs_range = range(epochs)
-    epochs_range = range(len(acc))
-
-    plt.figure(figsize=(8, 8))
-    plt.subplot(1, 2, 1)
-    plt.plot(epochs_range, acc, label='Training Accuracy')
-    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-    plt.legend(loc='lower right')
-    plt.title('Training and Validation Accuracy')
-
-    plt.subplot(1, 2, 2)
-    plt.plot(epochs_range, loss, label='Training Loss')
-    plt.plot(epochs_range, val_loss, label='Validation Loss')
-    plt.legend(loc='upper right')
-    plt.title('Training and Validation Loss')
+def plot_scores(scores):
+    train = scores[0]           # Accuracy
+    validation = scores[1]      # Loss
+    plt.subplot(1, 1, 1)
+    print(train)
+    print(validation)
+    plt.plot(train, color='blue', label='train')
+    plt.plot(validation, color='red', label='validation')
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
     plt.show()
+    print("Ok")
--- model_training.py
+++ model_training.py
@@ -1,8 +1,8 @@
-import h5py
 import tensorflow as tf
-from sklearn.model_selection import KFold, StratifiedKFold
+from keras_preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import KFold
 from tensorflow import keras
-from preprocessing import train_dataset, val_dataset, BATCH_SIZE, IMGS_PATH
+from preprocessing import train_dataset, val_dataset, BATCH_SIZE, IMGS_PATH, SIZE
 
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, \
@@ -10,11 +10,10 @@ from keras.layers import Conv2D, MaxPooling2D, \
     BatchNormalization
 
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from model_testing import  plot_scores
 
-import sklearn.model_selection as sklrn
 import pandas as pd
 import numpy as np
-
 
 def get_model(i: int):
     if i == 1:
@@ -52,11 +51,6 @@ def get_model(i: int):
         # model.summary()
         return model
 
-
-mcp_save = ModelCheckpoint('/tmp/ckeckpoint', save_best_only=True, monitor='val_loss', mode='min')
-reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, min_delta=1e-4, mode='min')
-early_stopping = EarlyStopping(monitor='val_loss', patience=15, verbose=0, mode='min')
-
 # history = get_model(1).fit(
 #     train_ds,
 #     epochs=3,
@@ -69,18 +63,65 @@ N_OF_EPOCHS = 5
 SHUFFLE_BUFFER_SIZE = 100
 
 
-def k_fold_cross_validation(model, n_folds=N_OF_FOLDS, epochs=N_OF_EPOCHS, batch_size=BATCH_SIZE):
+save_dir = '/saved_models/'
+reduce_lr_loss = ReduceLROnPlateau(monitor='loss', factor=0.1, patience=7, verbose=1, min_delta=1e-4, mode='min')
+early_stopping = EarlyStopping(monitor='loss', patience=15, verbose=0, mode='min')
+
+
+def get_model_name(i,k):
+    return 'model_'+str(i)+"_"+str(k)+'.h5'
+
+def k_fold_cross_validation(i):
+    #Training images and training labels
     X, y = train_dataset.next()
 
-    for train, test in KFold.split(X, y):
-        model.fit()
+    # For augmentation
+    traing_idg = ImageDataGenerator(width_shift_range=0.1,
+                             height_shift_range=0.1,
+                             zoom_range=0.3,
+                             fill_mode='nearest',
+                             horizontal_flip=True,
+                             rescale=1. / 255)
+    #For validation
+    valid_idg = ImageDataGenerator(rescale=1. /255)
+
+    scores = []
+    VALIDATION_ACCURACY = []
+    VALIDATION_LOSS = []
+    n_fold = 1
+
+    k_fold = KFold(n_splits=N_OF_FOLDS, shuffle= True)
+    for train, test in k_fold.split(X, y):
+        print('------------------------------------------------------------------------')
+        print(f'Training fold {n_fold} ...')
+
+        #Create callback to save model for current fold
+        mcp_save = ModelCheckpoint(save_dir + get_model_name(i,n_fold),
+                                   save_best_only=True,
+                                   monitor='loss',
+                                   mode='min',
+                                   verbose=1)
+
+        model = get_model(i)
+        tr = X[train]
+        te = y[train]
+        history = model.fit(
+            X[train],
+            y[train],
+            batch_size=BATCH_SIZE,
+            epochs=N_OF_EPOCHS,
+            callbacks=[mcp_save, reduce_lr_loss, early_stopping])
+
+        scores = model.evaluate(X[test], y[test], verbose=0)
+        # scores.append({'accuracy':np.average(history.history['accuracy']),
+        #                'loss':np.average(history.history['loss'])})
+
+        VALIDATION_ACCURACY.append(scores[1])
+        VALIDATION_LOSS.append(scores[0])
+
+        n_fold += 1
+
+    return [VALIDATION_ACCURACY,VALIDATION_LOSS]
 
 
-def save_dict():
-    dict = k_fold_cross_validation(model=get_model(1))
-
-    with h5py.File('dict_model1.h5', 'w') as hf:
-        hf.create_dataset("dict_model1",  data=dict)
-
-
-save_dict()
+plot_scores(k_fold_cross_validation(1))
